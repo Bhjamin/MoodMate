2fbfe2e86899c2d4ecaac1b88013baa2e0ee9dfa
Bhjamin <benjamin@hancock1.com>
2023-03-24 22:51:55 -0600

    Completed register function

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -1,3 +1,60 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const { SECRET } = process.env
+const { SECRET } = process.env;
+
+const { User } = require("../models/user");
+
+const bcrypt = require("bcryptjs");
+
+const jwt = require("jsonwebtoken");
+
+const createToken = (username, id) => {
+  return jwt.sign({ username, id }, SECRET, { expiresIn: "2 days" });
+};
+
+module.exports = {
+
+  register: async (req, res) => {
+
+    try {
+      const { username, password } = req.body;
+
+      let foundUser = await User.findOne({ where: { username } });
+
+      if (foundUser) {
+        res.status(400).send("This username is already in use");
+      } else {
+
+        const salt = bcrypt.genSaltSync(10)
+        const hash = bcrypt.hashSync(password, salt)
+
+        const newUser = await User.create({
+            username,
+            hashedPass: hash,
+        })
+
+        const token = createToken(newUser.username, newUser.id)
+
+        const exp = Date.now() + 1000 * 60 * 60 * 48
+
+        res.status(200).send({
+            username: newUser.username,
+            userId: newUser.id,
+            token,
+            exp
+        })
+
+      }
+    } catch (err) {
+        console.log(err)
+        res.status(400).send("Error in the register function");
+    }
+
+    
+  },
+
+  login: (req, res) => {
+    console.log("hit login");
+    res.sendStatus(200);
+  },
+};
--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 require('dotenv').config()
 
-const {SERVER_PORT} = process.env.SERVER_PORT
+const {SERVER_PORT} = process.env
 
 const { User, Entries } = require('./models/user')
 const { sequelize } = require('./util/database')
 
+const { register, login } = require('./controller/auth')
+
+User.hasMany(Entries)
+Entries.belongsTo(User)
 
 
 const express = require('express')
@@ -17,9 +21,13 @@ app.use(express.json())
 app.use(cors())
 
 
+app.post('/api/register', register )
+app.post('/api/login', login )
+
+
 sequelize.sync()
 .then(() => {
     app.listen(SERVER_PORT, () => {
-        console.log('Running on port 6655')
+        console.log(`Running on ${SERVER_PORT}`)
     })
 }).catch(err => console.log(err))
