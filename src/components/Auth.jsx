7df28c09f64c44de3087eaeed4066c29e1f6b1f9
Bhjamin <benjamin@hancock1.com>
2023-04-08 17:43:59 -0600

    improved SEO with lighthouse performace reader

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -10,81 +10,81 @@ const Auth = () => {
   const [password, setPassword] = useState("");
   const [register, setRegister] = useState(false);
   const [showPass, setShowPass] = useState("password");
-  const [quote, setQuote] = useState('')
-  const [author, setAuthor] = useState('')
-  const [error, setError] = useState('')
-  const navigate = useNavigate()
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
 
-  const {login, updateUsername} = useContext(AuthContext)
+  const { login, updateUsername } = useContext(AuthContext);
 
-  const authCtx = useContext(AuthContext)
+  const authCtx = useContext(AuthContext);
 
   const showPassHandler = () => {
     showPass === "password" ? setShowPass("text") : setShowPass("password");
   };
 
   useEffect(() => {
+    let randomNum = Math.floor(Math.random() * 1500);
 
-    let randomNum = Math.floor(Math.random() * 1500)
+    axios
+      .get("https://type.fit/api/quotes")
+      .then((res) => {
+        let QOTD = res.data[randomNum];
 
-    axios.get("https://type.fit/api/quotes")
-    .then(res => {
+        setQuote(QOTD.text);
+        setAuthor(QOTD.author);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-        let QOTD = res.data[randomNum]
+  const registerToggleHandler = (e) => {
+    e.preventDefault();
 
-        setQuote(QOTD.text)
-        setAuthor(QOTD.author)
-        
-    }).catch(err => console.log(err))
+    setRegister(!register);
+  };
 
-  }, [])
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
- const registerToggleHandler = (e) => {
-    e.preventDefault()
+    axios
+      .post(
+        register
+          ? "http://localhost:6655/register"
+          : "http://localhost:6655/login",
+        { username, password }
+      )
+      .then((res) => {
+        console.log(res.data);
 
-    setRegister(!register)
+        setUsername("");
+        setPassword("");
 
- }
+        updateUsername(res.data.username);
 
+        login(
+          res.data.token,
+          res.data.exp,
+          res.data.userId,
+          res.data.username,
+          res.data.points
+        );
 
- const handleSubmit = (e) => {
-  e.preventDefault()
+        navigate("/profile");
+      })
+      .catch((err) => {
+        setError(err.response.data);
 
-  axios.post(register ? 'http://localhost:6655/register' : 'http://localhost:6655/login' , {username, password})
-  .then(res => {
-    console.log(res.data)
-
-    setUsername('')
-    setPassword('')
-
-    
-    updateUsername(res.data.username)
-
-    login(res.data.token, res.data.exp, res.data.userId, res.data.username, res.data.points)
-
-    navigate('/profile')
-
-
-  })
-  .catch((err) => {
-
-    setError(err.response.data)
-
-    setTimeout(() => {
-      setError('')
-    }, 5000)
-
-  })
- }
-
-
+        setTimeout(() => {
+          setError("");
+        }, 5000);
+      });
+  };
 
   return (
     <div className=" overflow-x-hidden overflow-y-auto bg-base-100 w-full h-full flex flex-col items-center ">
       <Animation />
 
       <div className="w-full flex flex-row">
-
         <section className="w-2/5 flex items-center justify-center mb-10">
           <div
             className="tooltip tooltip-bottom before:max-w-xl before:leading-9 before:text-lg before:p-4 before:rounded-2xl "
@@ -98,7 +98,7 @@ const Auth = () => {
 
         <section className=" w-1/5 items-center justify-center">
           <div className="h-10">
-          <ErrorAnimation error={error} />
+            <ErrorAnimation error={error} />
           </div>
           <form className="flex flex-col">
             <div className="form-control m-4">
@@ -109,51 +109,66 @@ const Auth = () => {
                 type="text"
                 placeholder="Type here..."
                 className="input input-bordered "
-                onChange={e => setUsername(e.target.value)}
+                onChange={(e) => setUsername(e.target.value)}
                 value={username}
               />
             </div>
 
             <div className="form-control m-4">
-              <label className="label-text m-1">
+              <label className="label-text m-1" htmlFor="password">
                 <span className="label-text text-lg ">Password</span>
               </label>
               <div className="flex flex-row items-center ">
                 <input
+                  id="password"
                   type={showPass}
                   placeholder="Type here..."
                   className="input input-bordered "
-                  onChange={e => setPassword(e.target.value)}
+                  onChange={(e) => setPassword(e.target.value)}
                   value={password}
                 />
-                <section className=" flex flex-row items-center ml-2 ">
+
+                <label className="  flex flex-row items-center ml-2 ">
                   <input
                     type="checkbox"
                     className="toggle toggle-sm"
                     onChange={showPassHandler}
                   />
                   <p className="text-xs ml-2">Show password</p>
-                </section>
+                </label>
               </div>
             </div>
-            <br/>
+            <br />
             <div className=" w-full flex flex-col justify-center items-center">
-            <button onClick={(e) => {handleSubmit(e)}} className="btn btn-primary w-20 ">{register ? "register" : "login"}</button>
-            <br/>
-            <br/>
-            <button className="btn btn-sm" onClick={(e) => {registerToggleHandler(e)}} >Click here to {register ? "login" : "register"}</button>
+              <button
+                onClick={(e) => {
+                  handleSubmit(e);
+                }}
+                className="btn btn-primary w-20 "
+              >
+                {register ? "register" : "login"}
+              </button>
+              <br />
+              <br />
+              <button
+                className="btn btn-sm"
+                onClick={(e) => {
+                  registerToggleHandler(e);
+                }}
+              >
+                Click here to {register ? "login" : "register"}
+              </button>
             </div>
           </form>
         </section>
 
         <section className="w-2/5 flex flex-col  items-center justify-center">
-            <div className="flex flex-col items-center text-center w-3/5 mb-10">
+          <div className="flex flex-col items-center text-center w-3/5 mb-10">
             <p>"{quote}"</p>
-            <br/>
+            <br />
             {author ? <p>- {author}</p> : <p>- Unknown</p>}
-            </div>
+          </div>
         </section>
-
       </div>
     </div>
   );
